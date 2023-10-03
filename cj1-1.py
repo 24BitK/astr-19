c953624e8a6abf81cd47897c61de33835d390db0
Kali <kamdanie@ucsc.edu>
2023-10-02 21:55:05 -0700

    Update cj1-1.py
    Session 1: full name, pronouns, two sentences.

--- cj1-1.py
+++ cj1-1.py
@@ -1,1 +1,14 @@
+# Session 1 Prompt: Write a Python program to print out your 
+# full name, your preferred pronouns (optional), and two sentences 
+# about your favorite movie and your favorite food.
+def print_Kali():
+    print("Kali Daniels")
+    print("he/they")
+    print("My favorite moveie is tie between Scott Pilgrim and The Nightmare before Christmas.")
+    print("My favorite food is probably Filipino food, I especially like to cook Chicken Adobo.")
+    
+def main():
+    print_Kali()
 
+if __name__=="__main__":
+    main()
